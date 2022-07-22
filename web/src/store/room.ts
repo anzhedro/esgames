924f9c7a513beae8b0408f958cc2065e400218df
smiface <smiface.com>
2022-07-22 05:07:35 +0300

    fix room store syntax error

--- web/src/store/room.ts
+++ web/src/store/room.ts
@@ -4,70 +4,66 @@ import { IPlayer } from "../utils/types";
 const AliasSettings = {
   settings: {
     roundTime: [30, 60, 90],
-    roundsCount: [2,3,4],
+    roundsCount: [2, 3, 4],
     difficulty: ["easy", "medium", "hard"],
     wordsLanguage: ["en", "ru"],
     // необязательное количетсво команд
-    teamsCount: [2,3,4],
-  }
-}
+    teamsCount: [2, 3, 4],
+  },
+};
 
 const SettingsAliasToBack = {
-    type: 'create_game',
-    gameName: 'alias',
-    roundTime: 30,
-    roundsCount: 4,
-    difficulty: "easy",
-    wordsLanguage: "ru",
-    teamsCount: 2,
-}
+  type: "create_game",
+  gameName: "alias",
+  roundTime: 30,
+  roundsCount: 4,
+  difficulty: "easy",
+  wordsLanguage: "ru",
+  teamsCount: 2,
+};
 
 // настроящие настройки
 // текущие настройки
 
 const SocketCurrentGame = {
-    type: 'game_state',
-    gameName: 'alias',
-    roundTime: 30,
-    roundsCount: 4,
-    difficulty: "easy",
-    wordsLanguage: "ru",
-    teamsCount: 2,
+  type: "game_state",
+  gameName: "alias",
+  roundTime: 30,
+  roundsCount: 4,
+  difficulty: "easy",
+  wordsLanguage: "ru",
+  teamsCount: 2,
 
-    teams: [
-// team 1
+  teams: [
+    // team 1
     {
       score: 1,
-      players: [{
-        name: 'sloth',
-        avatarId: 1
-      },
-      {
-        name: 'sloth',
-        avatarId: 2
-      }
-    ]
-  }
-     
+      players: [
+        {
+          name: "sloth",
+          avatarId: 1,
+        },
+        {
+          name: "sloth",
+          avatarId: 2,
+        },
+      ],
+    },
+  ],
 
-    ],
+  // штуки которые меняются
 
-    // штуки которые меняются
+  // какой по счету раунд
+  currentRound: 3,
 
-    // какой по счету раунд
-    currentRound: 3,
+  currentWord: "",
 
-    currentWord: '',
+  currentTeam: 1,
+  // какой по счету игрок в команде объясняет слова
+  currentPlayer: 1,
 
-    currentTeam: 1,
-    // какой по счету игрок в команде объясняет слова
-    currentPlayer: 1,
-
-    teamScore: 
-
-
-}
-
+  teamScore: [],
+};
 
 export class Room {
   ws: WebSocket;
