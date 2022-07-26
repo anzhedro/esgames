4633e1b84509166bd0698c2a5a094372142f060c
Yauhen Androsik <androsikei95@gmail.com>
2022-07-27 00:48:42 +0200

    Added Dockerfile
    Co-authored-by: Pavel Sychev <luckygeck@google.com>

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM golang:1.18.4-alpine3.16 AS build-server
+WORKDIR /app
+COPY go.mod go.sum /app/
+COPY api /app/api
+RUN go mod download
+RUN find .
+RUN go build -o server api/cmd/main.go
+
+FROM node:18-alpine3.16 AS build-front
+WORKDIR /app
+COPY ./web/package*.json ./web/tsconfig.json ./
+RUN npm install
+COPY ./web ./
+RUN npm run build
+
+FROM alpine:latest
+RUN apk --no-cache add ca-certificates
+WORKDIR /root/
+EXPOSE 8000
+COPY --from=build-front /app/dist ./dist
+COPY --from=build-server /app/server ./server
+RUN find /root/
+CMD [ "./server", "--port=8000" ]
+
+# docker build --tag docker-go-server .
+# docker run --rm -it -p 8000:8000 docker-go-server
--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -9,13 +11,20 @@ import (
 	"github.com/anzhedro/esgames/api/reaction"
 )
 
+var (
+	port = flag.Int("port", 8000, "port to listen on")
+)
+
 func main() {
+	flag.Parse()
+
 	games := map[string]api.StartGameFn{
 		reaction.Name: reaction.NewGame,
 		hat.Name:      hat.NewGame,
 	}
 	srv := api.NewServer(games)
+	http.Handle("/", http.FileServer(http.Dir("dist")))
 	http.HandleFunc("/ws", srv.Handle)
-	log.Println("Starting server on :8000")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Printf("Starting server on :%d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 }
