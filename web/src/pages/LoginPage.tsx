692b32f9888a8cc55b86f31c2256dfdf72a4b6b4
smiface <smiface.com>
2022-08-01 20:13:27 +0300

    add  Timer

--- web/src/pages/LoginPage.tsx
+++ web/src/pages/LoginPage.tsx
@@ -4,6 +4,7 @@ import { LoginCard } from '../components/LoginCard';
 import { currentLanguage, localizationMap } from '../store/localization';
 import { appState, room } from '../store/state';
 import { Spinner } from '../components/Spinner';
+import { Timer } from '../components/Timer';
 
 export const LoginPage = () => {
   const navigate = useNavigate();
